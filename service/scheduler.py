a8e3fcc5c98a6e0a45b8112363227c504e3fbc79
jramosecommerce <jramosecommerce@gmail.com>
2025-07-25 16:49:38 -0300

    Update scheduler.py

--- service/scheduler.py
+++ service/scheduler.py
@@ -1,5 +1,12 @@
-from telegram.ext import Application from handlers.sugestao_handler import sugestao_handler from datetime import time
+from telegram.ext import Application
+from handlers.sugestao_handler import sugestao_handler
+from datetime import time
 
-def start_scheduler(app: Application): app.job_queue.run_daily(scheduled_suggestion, time(hour=10, minute=0))
+def start_scheduler(app: Application):
+    app.job_queue.run_daily(scheduled_suggestion, time(hour=10, minute=0))
 
-def scheduled_suggestion(context): chat_id = os.getenv("GRUPO_ID") context.bot.send_message(chat_id=chat_id, text="Enviando sugestões automáticas...") context.args = [] sugestao_handler(update=None, context=context)
+def scheduled_suggestion(context):
+    chat_id = os.getenv("GRUPO_ID")
+    context.bot.send_message(chat_id=chat_id, text="Enviando sugestões automáticas...")
+    context.args = []
+    sugestao_handler(update=None, context=context)
