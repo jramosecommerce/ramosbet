a189399a198f33bcec8a19e28f4bfee26dc5ec71
jramosecommerce <jramosecommerce@gmail.com>
2025-07-25 10:17:42 -0300

    Delete service directory

--- service/prediction_service.py
+++
@@ -1,5 +0,0 @@
-def get_predictions():
-    return [
-        {"match": "Atlético-MG x Corinthians", "tip": "Mais de 2.5 gols", "odds": 1.90},
-        {"match": "Flamengo x Palmeiras", "tip": "Ambos marcam", "odds": 1.75}
-    ]
--- service/stats_service.py
+++
@@ -1,5 +0,0 @@
-def get_past_match_stats():
-    return [
-        {"match": "Flamengo x Vasco", "goals": "2-1", "corners": 10, "cards": 5},
-        {"match": "Palmeiras x Santos", "goals": "1-1", "corners": 8, "cards": 3}
-    ]
