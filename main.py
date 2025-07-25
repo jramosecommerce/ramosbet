02aa3051cb1d61a76414cb7e4e00e3b3d8b314b5
jramosecommerce <jramosecommerce@gmail.com>
2025-07-25 16:30:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
-import logging
-import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from handlers.start_handler import start_handler
-from handlers.hoje_handler import hoje_handler
-from handlers.estatisticas_handler import estatisticas_handler
-from handlers.sugestao_handler import sugestao_handler
-
-from service.scheduler import start_scheduler
-from dotenv import load_dotenv
-
-load_dotenv()
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+from telegram.ext import (
+    ApplicationBuilder, CommandHandler, CallbackQueryHandler,
+    ContextTypes
 )
+from handlers.start_handler import start
+from handlers.hoje_handler import hoje_handler
+from handlers.estatisticas_handler import estatisticas_handler, handle_estatisticas_selection
+from handlers.sugestao_handler import sugestao_handler
+from service.scheduler import start_scheduler, scheduled_suggestion
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start_handler))
+app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("hoje", hoje_handler))
 app.add_handler(CommandHandler("estatisticas", estatisticas_handler))
+app.add_handler(CallbackQueryHandler(handle_estatisticas_selection))
 app.add_handler(CommandHandler("sugestao", sugestao_handler))
 
-start_scheduler(app)  # Inicia o agendador
-print("Bot em execução...")
+start_scheduler(app)
+
+print("🤖 Bot está rodando...")
 app.run_polling()
