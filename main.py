674eb68d98c1ac5046392f09052034cb5c72ac4e
jramosecommerce <jramosecommerce@gmail.com>
2025-07-27 20:25:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
-import asyncio
 from telegram.ext import ApplicationBuilder
-from handlers.start_handler import start
-from handlers.hoje_handler import registrar_handlers_hoje
-from handlers.estatisticas_handler import registrar_handlers_estatisticas
-from handlers.sugestao_handler import registrar_handlers_sugestao
-from telegram.ext import CommandHandler
+from handlers.hoje import registrar_handlers_hoje
+from handlers.estatisticas import registrar_handlers_estatisticas
+from handlers.sugestao import registrar_handlers_sugestao
+import os
 
-# Substitua pelo seu token do BotFather
-TOKEN = "8242348358:AAES95eDSqPFGoyZ7vmgZFolEBSClE40O_Y"
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
-    # /start
-    app.add_handler(CommandHandler("start", start))
-
-    # /hoje
     registrar_handlers_hoje(app)
-
-    # /estatisticas
     registrar_handlers_estatisticas(app)
-
-    # /sugestao
     registrar_handlers_sugestao(app)
 
-    print("✅ Bot está rodando...")
+    print("✅ Bot iniciado...")
     await app.run_polling()
 
 if __name__ == "__main__":
-    import nest_asyncio
-    nest_asyncio.apply()
-
+    import asyncio
     asyncio.run(main())
