63adeb79a701c95c48508995d54de23e46d42bfb
jramosecommerce <jramosecommerce@gmail.com>
2025-07-27 20:23:25 -0300

    Update hoje_handler.py

--- handlers/hoje_handler.py
+++ handlers/hoje_handler.py
@@ -1,13 +1,11 @@
 from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
-from service.flashscore_scraper import obter_jogos_do_dia
+from service.match_service import obter_jogos_do_dia
 from asyncio import to_thread
 
 async def hoje_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         await update.message.reply_text("📅 Buscando jogos reais de hoje...")
-
-        # Executa função síncrona em thread separada
         jogos = await to_thread(obter_jogos_do_dia)
 
         if not jogos:
@@ -16,9 +14,10 @@ async def hoje_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         resposta = "*Jogos de hoje:*\n\n"
         for jogo in jogos:
-            resposta += f"🏟️ {jogo}\n"
+            nome = f"{jogo['time_casa']} x {jogo['time_fora']}"
+            resposta += f"🏟️ {nome}\nID da Partida: `{jogo['id_partida']}`\n\n"
 
-        await update.message.reply_text(resposta, parse_mode="Markdown", disable_web_page_preview=True)
+        await update.message.reply_text(resposta, parse_mode="Markdown")
 
     except Exception as e:
         await update.message.reply_text(f"❌ Erro ao buscar jogos de hoje: {e}")
